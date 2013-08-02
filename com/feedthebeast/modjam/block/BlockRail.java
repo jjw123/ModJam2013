856e2412c90458a52cac1fda06c3f2d4be2aa610
Sean <unvcustoms@gmail.com>
2013-08-02 04:45:01 -0500

    Automatic Commit

--- com/feedthebeast/modjam/block/BlockRail.java
+++ com/feedthebeast/modjam/block/BlockRail.java
@@ -1,5 +1,7 @@
 package com.feedthebeast.modjam.block;
 
+import java.util.List;
+
 import com.feedthebeast.modjam.entity.TestEntity;
 
 import cpw.mods.fml.common.FMLLog;
@@ -7,6 +9,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.IBlockAccess;
@@ -121,5 +124,12 @@ public class BlockRail extends ModBlock {
     		this.setBlockBounds(0, 0, 0, 16, 16, 16);
     		break;
     	}
+    }
+    
+
+    @Override
+    public void addCollisionBoxesToList(World par1World, int par2, int par3, int par4, AxisAlignedBB par5AxisAlignedBB, List par6List, Entity par7Entity) {
+        this.setBlockBoundsBasedOnState(par1World, par2, par3, par4);
+        super.addCollisionBoxesToList(par1World, par2, par3, par4, par5AxisAlignedBB, par6List, par7Entity);
     }
 }
